26e7ef2e2d6ae34ded20d84916267f5ed7486d33
Doogie <doogie.stickrd@gmail.com>
2017-09-10 15:51:02 -0300

    Object

--- app/products/product.ts
+++ app/products/product.ts
@@ -1,6 +1,9 @@
 export interface Product {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
+    id: number;
+    name: string;
+    description: string;
+    address: string;
+    locality: string;
+    state: string;
+    country: string;      
 }
