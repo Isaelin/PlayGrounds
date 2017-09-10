e365f34ac7859e1ea2a499e040a378bf7fe51504
Doogie <doogie.stickrd@gmail.com>
2017-09-10 02:00:32 -0300

    Display Example Using Playgrounds

--- app/products/product-data.ts
+++ app/products/product-data.ts
@@ -2,17 +2,26 @@ import { Product } from './product';
 
 export const PRODUCT_ITEMS: Product[] = [{
     id: 1,
-    name: 'Scissors',
-    description: 'use this to cut stuff',
-    price: 4.99
+    name: 'Playground 1',
+    description: 'Description for Playground 1',
+    address: 'Test Avenue 2532',
+    locality: 'Locality Test',
+    state: 'State Test',
+    country: 'Country Test'    
   }, {
     id: 2,
-    name: 'Steak Knives',
-    description: 'use this to eat food with',
-    price: 10.99
+    name: 'Playground 2',
+    description: 'Description for Playground 2',
+    address: 'Test Avenue 2535',
+    locality: 'Locality Test 2',
+    state: 'State Test 2',
+    country: 'Country Test 2'
   }, {
     id: 3,
-    name: 'Shot Glass',
-    description: 'use this to take shots',
-    price: 5.99
+    name: 'Playground 3',
+    description: 'Description for Playground 3',
+    address: 'Test Avenue 2538',
+    locality: 'Locality Test 3',
+    state: 'State Test 3',
+    country: 'Country Test 3'
   }]
